c646c47ea8227c81e2684f5614d7493fd6d9f423
Kei Kawaguchi <k.kawaguchi@cisys.co.jp>
2015-12-12 14:50:38 +0900

    class分けた

---
+++ AchievementReports/AchievementRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace AchievementReports
+{
+    public class AchievementRepository
+    {
+        private IDbConnection conn;
+        public AchievementRepository(IDbConnection conn)
+        {
+            this.conn = conn;
+        }
+
+        public void Insert(Achievement achievement)
+        {
+            String sql;
+            sql = "";
+            sql = "INSERT INTO 実績(日付,人ID,実績分,会ID) VALUES(#" + achievement.date + "#," + achievement.personID + "," + achievement.time + "," + achievement.meetingID + ");";
+
+            IDbCommand command = this.conn.CreateCommand();
+            command.CommandText = sql;
+            command.ExecuteNonQuery();
+        }
+    }
+}
---
+++ AchievementReports/Models.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AchievementReports
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public string kana { get; set; }
+        public int personID { get; set; }
+    }
+
+    public class Meeting
+    {
+        public string descripshon { get; set; }
+        public int dayDuty { get; set; }
+        public DateTime date { get; set; }
+        public int meetingID { get; set; }
+    }
+
+    public class Achievement
+    {
+        public int achievementID { get; set; }
+        public int personID { get; set; }
+        public int meetingID { get; set; }
+        public DateTime date { get; set; }
+        public int time { get; set; }
+    }
+}
